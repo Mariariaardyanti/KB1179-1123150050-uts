7014c3a68a459e21c371eee09acb9075431443d3
Mariariaardyanti <riaardyanti172gmail.com>
2025-11-07 22:25:44 +0700

    update: add image inside indigo circle

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -6,13 +6,18 @@ class MySplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Container(
           height: 200,
           width: 200,
           decoration: const BoxDecoration(
             shape: BoxShape.circle,
-            color: Colors.blueAccent,
+            color: Colors.indigo,
+            image: DecorationImage(
+              image: AssetImage("assets/images/1.jpg"),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
       ),
