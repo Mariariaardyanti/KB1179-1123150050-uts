66fdd432c6e3d372dd20f02c1ee45f1a9d17bf11
Mariariaardyanti <riaardyanti172gmail.com>
2025-11-08 10:35:28 +0700

    feat: add title and subtitle login

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -8,15 +8,23 @@ class Login extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Center(
-          child: Container(
-            padding: const EdgeInsets.all(20.0),
-            child: const Column(
-              children: [
-                SizedBox(height: 40),
-                Text("Login Screen"),
-              ],
-            ),
+         child: const Column(
+  children: [
+    SizedBox(height: 40),
+      Icon(Icons.shopping_bag, size: 50, color: Colors.indigo),
+        SizedBox(height: 20),
+          Text(
+      "Welcome Back!",
+      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    ),
+    SizedBox(height: 8),
+       Text(
+          "Silakan login untuk melanjutkan",
+            style: TextStyle(fontSize: 15, color: Colors.black54),
+              ),
+            ],
           ),
+
         ),
       ),
     );
